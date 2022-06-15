dc657ab34e7d2e9d5859cbba998f4f86e4ac1ca9
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-06-15 14:57:25 -0300

    Puntos A, B y C terminados en EstudiantesAlt

--- EstudiantesALT.sol
+++ EstudiantesALT.sol
@@ -1,82 +1,98 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+    pragma solidity ^0.8.13;
 
-contract Estudiante
-{
-    string private _nombre;
-    string private _apellido;
-    string private _curso;
-    address private _docente;
-
-    mapping(string => uint256) private _notas_materia;
-    
-    mapping(string => uint256)[4] private _bim_mat_notas;
-
-    string[] private _materias;
-    uint256[4] private _bimestres;
-
-    constructor(string memory nombre_, string memory apellido_, string memory curso_)
+    contract Estudiante
     {
-        _nombre = nombre_;
-        _apellido = apellido_;
-        _curso = curso_;
-
-        _docente = msg.sender;
-    }
-
-    function get_apellido() public view returns(string memory)
-    {
-        return _apellido;
-    }
-
-    function get_nombre_completo() public view returns (string memory)
-    {
-        return string.concat(_nombre," ", _apellido);
-    }
-
-    function get_curso() public view returns(string memory)
-    {
-        return _curso;
-    }
-
-    function set_notamateria(uint256 nota_, string memory materia_, uint256 bim_) public
-    {
-        require(msg.sender == _docente, "Solo el docente registrado puede asignar notas");
-        //_notas_materia[materia_] = nota_;
-
-        _bim_mat_notas[bim_][materia_] = nota_;
+        string private _nombre;
+        string private _apellido;
+        string private _curso;
+        address private _docente;
         
-        _bimestres[bim_] = _materias[materia_];
-    }
+        mapping(string => uint256)[4] private _bim_mat_notas;
 
-    function get_notamateria(string memory materia_, uint256 bim_) public view returns(uint256)
-    {
-        return _bim_mat_notas[bim_][materia_];
-    }
+        string[] private _materias;
+        address[] private _docentes_asignados;
 
-    function aprobo(string memory materia_, uint256 bim_) public view returns(bool)
-    {
-        if(_bim_mat_notas[bim_][materia_] >= 60)
+        event log_nota_agregada(string message, address indexed sender, uint256 bim, string materia, uint256 nota);
+
+        constructor(string memory nombre_, string memory apellido_, string memory curso_)
         {
-            return true;
+            _nombre = nombre_;
+            _apellido = apellido_;
+            _curso = curso_;
+
+            _docente = msg.sender;
         }
-        return false;
-    }
 
-    function promedio(uint256 bimestre_) public view returns(uint256)
-    {
-        uint256 _promedio = 0;
-
-        for(uint256 i = 0; i < 4; i++)
+        function get_apellido() public view returns(string memory)
         {
-            for(uint256 i = 0; i < _materias.length; i++)
+            return _apellido;
+        }
+
+        function get_nombre_completo() public view returns (string memory)
+        {
+            return string.concat(_nombre," ", _apellido);
+        }
+
+        function get_curso() public view returns(string memory)
+        {
+            return _curso;
+        }
+
+        function set_notamateria(uint256 nota_, string memory materia_, uint256 bim_) public
+        {
+            bool _isDocente = false;
+            for(uint256 i = 0; i < _docentes_asignados.length; i++)
             {
-                _promedio += _bim_mat_notas[bimestre_][_materias[i]];
+                if(_docentes_asignados[i] == msg.sender)
+                {
+                    _isDocente = true;
+                    break;
+                }
             }
-        }
-        
 
-        _promedio /= _materias.length;
-        return _promedio;
+            require(msg.sender == _docente || _isDocente == true, "Solo el docente registrado puede asignar notas");
+            
+            _bim_mat_notas[bim_][materia_] = nota_;
+            
+            _materias.push(materia_);
+
+            emit log_nota_agregada("Nota agregada con estas caracteristicas: ", msg.sender, bim_, materia_, nota_);
+        }
+
+        function set_docentes_asignados(address adress_docente_) public
+        {
+            require(msg.sender == _docente, "Solo el docente administrador puede asignar otros docentes");
+            _docentes_asignados.push(adress_docente_);
+        }
+
+        function get_notamateria(string memory materia_, uint256 bim_) public view returns(uint256)
+        {
+            return _bim_mat_notas[bim_][materia_];
+        }
+
+        function aprobo(string memory materia_, uint256 bim_) public view returns(bool)
+        {
+            if(_bim_mat_notas[bim_][materia_] >= 60)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        function promedio() public view returns(uint256)
+        {
+            uint256 _promedio = 0;
+
+            for(uint256 i = 0; i < 4; i++)
+            {
+                for(uint256 j = 0; j < _materias.length; j++)
+                {
+                    _promedio += _bim_mat_notas[i][_materias[j]];
+                }
+            }
+
+            _promedio /= _materias.length;
+            return _promedio;
+        }
     }
-}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TP_Solidity_Estudiantes
  
+Link a Etherscan Estudiantes.sol : https://rinkeby.etherscan.io/address/0xD2C8975131043Cd0562de058e085f1AEC47a269B
+
+A) Modificamos el smart contract para que acepte notas de los 4 bimestres utilizando un array de mappings para
+asi poder acceder a las notas y materias en cada posición del array individualmente por cada bimestre.
+
+B) Para hacerto, primero creamos un array del tipo "adress" llamado "_docentes_asignados". Luego, creamos una función la
+cuál solamente puede correr el docente administrador (también podría ser que cualquier docente dentro del array pueda
+llamar esta función) que toma un adress y lo agrega al array _docentes_asignados. Por último, al poner notas nos fijamos
+si el msg.sender está dentro del array de _docentes_asignados.
+
+C) Logramos esto, creando un evento que toma: message, address , bim, materia, nota.
+Al asignar una nota, emitiamos el evento utilizando las variables adecuadas (mensaje dice que se emitió).
+
+d) Todos los puntos aplicados en el archivo estudiantesAlt.sol.
+
+Link a Etherscan EstudiantesAlt.sol : https://rinkeby.etherscan.io/address/0x3574FA510397e981769357cF74FDcAfa81137FA0
